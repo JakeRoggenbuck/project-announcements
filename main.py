25475aaa462d1e45b590a2bae11aca1a674352f4
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-09-10 14:09:04 -0700

    Get basic Repo class

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-import requests
+class Repo:
+    base_github_url = "https://github.com"
+    base_raw_url = "https://raw.githubusercontent.com"
 
-USER = "JakeRoggenbuck"
-NAME = "auto-clock-speed"
+    def __init__(self, username: str, project_name: str):
+        self.username = username
+        self.project_name = project_name
 
-URL = "https://github.com/JakeRoggenbuck/auto-clock-speed/"
+    def get_github_url(self):
+        return f"{self.base_github_url}/{self.username}/{self.project_name}"
 
-def split_url(url: str):
-    """Split a github url into a username and a project_name"""
-    _, _, username, project_name = [x for x in url.split("/") if x != ""]
-    return username, project_name
+    def get_readme_url(self):
+        return f"{self.base_raw_url}/{self.username}/{self.project_name}/main/README.md"
 
+    @staticmethod
+    def repo_from_url(url: str):
+        """Split a github url into a username and a project_name"""
+        _, _, username, project_name = [x for x in url.split("/") if x != ""]
+        return Repo(username, project_name)
 
-def convert_to_readme_url(username: str, project_name: str):
-    return "https://raw.githubusercontent.com/{username}/{project_name}/main/README.md"
+    def __repr__(self):
+        return f"{self.username}/{self.project_name}"
+
 
 if __name__ == "__main__":
-    print(split_url(URL))
+    repo = Repo.repo_from_url("https://github.com/JakeRoggenbuck/jai")
+    print(repo)
+
+    print(repo.get_github_url())
+    print(repo.get_readme_url())
